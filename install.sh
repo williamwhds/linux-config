e38176daaf0da141d978f3389718d8606c0d18e1
William Oliveira <106339584+williamwhds@users.noreply.github.com>
2024-03-22 21:12:49 +0000

    add install.sh

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# update Debian
+sudo apt-get update && sudo apt-get upgrade -y
+
+# xorg, xinit and i3
+sudo apt-get install xorg xinit i3 -y
+
+# alacritty
+sudo apt-get install alacritty -y
+
+# nitrogen
+sudo apt-get install nitrogen -y
+
+# firefox-esr
+sudo apt-get install firefox-esr -y
+
+# thunar
+sudo apt-get install thunar -y
+
+# npm
+sudo apt-get install npm -y
+
+# nodejs
+sudo apt-get install nodejs -y
+
+# zsh
+sudo apt-get install zsh -y
+
+# ripgrep
+sudo apt-get install ripgrep -y
+
+# rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
