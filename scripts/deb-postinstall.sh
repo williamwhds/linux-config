32a33df8a7a5c52a2d935ccf8523560651bf826f
William Oliveira <williamwhds@localhost.localdomain>
2024-03-22 22:37:50 -0300

    update deb-postinstall.sh

--- scripts/deb-postinstall.sh
+++ scripts/deb-postinstall.sh
@@ -5,7 +5,8 @@ cp -r ../dotfiles/. ~/
 
 # copy applications from this repo to home directory
 mkdir -p ~/Applications
-cp -r ../applications/ ~/Applications/
+cp -r ../applications/ ~/
+mv ~/applications ~/Applications
 
 # making symbolic links from applications to /usr/local/bin
 sudo ln -s ~/Applications/* /usr/local/bin/
