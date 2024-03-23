2c2ff60168cdc2788a90324693bd5755093dfaad
William Oliveira <202211240022@ifba.edu.br>
2024-03-22 23:48:40 -0300

    update deb-postinstall.sh

--- scripts/deb-postinstall.sh
+++ scripts/deb-postinstall.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# Stops sudo from changing $HOME to /root/
+USER_HOME=$(eval echo ~$USER)
+
 # copy dotfiles from this repo to home directory
-cp -r ../dotfiles/. ~/
+cp -r ../dotfiles/. $USER
 
 # copy applications from this repo to home directory
-mkdir -p ~/Applications
-cp -r ../applications/ ~/
-mv ~/applications ~/Applications
+cp -r ../applications $USER
+mv $USER/applications $USER/Applications
 
 # making symbolic links from applications to /usr/local/bin
 sudo ln -s ~/Applications/* /usr/local/bin/
